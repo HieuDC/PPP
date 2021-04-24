10292ff6acddd652167735a999dfa3a325438b3a
Do Chi Hieu <dochihieu1994@gmail.com>
2021-04-25 00:27:22 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
       {
         name: 'ctv-assist',
-        script: 'npx',
-        args: 'serve -s build -l 3000 -n',
+        script: 'node_modules/@angular/cli/bin/ng',
+        args: 'serve --host 0.0.0.0',
         interpreter: 'none',
         env: {
           NODE_ENV: 'development',
